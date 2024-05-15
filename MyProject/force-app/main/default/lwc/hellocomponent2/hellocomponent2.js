1d3a1eec5ad06396d3dde83d3fed82c94838f41a
MuserrefOzturk <118962940+MuserrefOzturk@users.noreply.github.com>
2024-05-15 16:44:26 +0300

    c2

--- MyProject/force-app/main/default/lwc/hellocomponent2/hellocomponent2.js
+++ MyProject/force-app/main/default/lwc/hellocomponent2/hellocomponent2.js
@@ -1,11 +1,29 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track} from 'lwc';
 
 export default class Hellocomponent2 extends LightningElement {
 
-titletext='Hello Component!!';
-footertext='With Sincere';
+    // like an object
+
+@track cardtext={
+
+    title:'Hello Component!!',
+    footer:'With Sincere',
+    body: 'Here is my component.'
+}
+
 
 handler(){
-    this.titletext='Button Clicked..';
+    this.cardtext.title='Button Clicked..';
+    this.cardtext.footer= 'footer changed';
+    this.cardtext.body='body chanced';
    }
+
+//  OR
+
+/*this.cardtext={
+    title:'Button Clicked..',
+    footer:'footer changed',
+    body: 'body chanced'
+} */
+
 }
