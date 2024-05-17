81a16238bea6f712081faaba4ecc6aa5c60c33ec
MuserrefOzturk <118962940+MuserrefOzturk@users.noreply.github.com>
2024-05-17 09:40:21 +0300

    c3

--- MyProject/force-app/main/default/lwc/hellochild/hellochild.js
+++ MyProject/force-app/main/default/lwc/hellochild/hellochild.js
@@ -2,6 +2,15 @@ import { LightningElement, api} from 'lwc';
 
 export default class Hellochild extends LightningElement {
 
+    // --Properties--
     @api titletext;
     @api footertext;
+
+    // We can alsa add mothods...
+
+   @api changetitle(){
+
+  this.titletext='method run';
+    }
+
 }
--- MyProject/force-app/main/default/lwc/helloparent/helloparent.js
+++ MyProject/force-app/main/default/lwc/helloparent/helloparent.js
@@ -1,5 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class Helloparent extends LightningElement {
-    
+    clickhandler(){
+
+        this.template.querySelector('c-hellochild').changetitle();
+    }
 }
