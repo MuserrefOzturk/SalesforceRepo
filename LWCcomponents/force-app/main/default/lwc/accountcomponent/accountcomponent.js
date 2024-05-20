94e4e61b393c0967ca631a04f69c8032bd231bdb
MuserrefOzturk <118962940+MuserrefOzturk@users.noreply.github.com>
2024-05-20 13:45:05 +0300

    c3

--- LWCcomponents/force-app/main/default/lwc/accountcomponent/accountcomponent.js
+++ LWCcomponents/force-app/main/default/lwc/accountcomponent/accountcomponent.js
@@ -1,6 +1,6 @@
 import { LightningElement ,wire} from 'lwc';
 import getAccList from "@salesforce/apex/accountLWCcontroller.retrieve";
-export default class Accountcomponentwire extends LightningElement {
+export default class Accountcomponent extends LightningElement {
 
     // wire ile
 @wire(getAccList) Accounts;
---
+++ LWCwithAPEX/force-app/main/default/lwc/inputcomponent/inputcomponent.js
@@ -0,0 +1,38 @@
+import { LightningElement,wire,track } from 'lwc';
+
+import getList  from "@salesforce/apex/apexinlwc.retrive";
+
+export default class Inputcomponent extends LightningElement {
+
+@track multiacc;
+
+searchtext;
+
+@wire(getList,{searchterm:'$searchtext'}) Accounts;
+
+
+renderedCallback() {
+    console.log(this.Accounts);
+}
+
+
+
+changehandler(event){
+this.searchtext=event.target.value;
+console.log(this.searchtext);
+}
+
+connectedCallback(){
+/*
+getList()
+
+.then(data => {
+    this.multiacc=data;
+})
+.catch(error => {
+    console.error(error);
+});
+*/
+
+}
+}
