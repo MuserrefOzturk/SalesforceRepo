70488ec453bb7ed54aa3e8df740bdea78184b070
MuserrefOzturk <118962940+MuserrefOzturk@users.noreply.github.com>
2024-05-26 09:08:44 +0300

    c3

---
+++ LWCwithAPEX/force-app/main/default/lwc/recordForm/recordForm.js
@@ -0,0 +1,13 @@
+import { LightningElement,api } from 'lwc';
+
+import FIRSTNAME from '@salesforce/schema/Contact.FirstName';
+import LASTNAME from '@salesforce/schema/Contact.LastName';
+import PHONE from '@salesforce/schema/Contact.Phone';
+import EMAIL from '@salesforce/schema/Contact.Email';
+export default class RecordForm extends LightningElement {
+
+    fields = [FIRSTNAME,LASTNAME,PHONE,EMAIL];
+
+    @api recordId;
+    @api objectApiName;
+}
